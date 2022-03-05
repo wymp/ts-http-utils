29b041383fb00e4fa4777225e0a94c6d0e44a6ea
Kael Shipman <kael.shipman@gmail.com>
2022-03-04 20:09:53 -0600

    Updated versions

--- src/ApiUtils.ts
+++ src/ApiUtils.ts
@@ -1,5 +1,6 @@
 import {
   SimpleLoggerInterface,
+  SimpleHttpRequestHandlerInterface,
   SimpleHttpRequestHandlerBasicInterface,
 } from "@wymp/ts-simple-interfaces";
 import { Api, Auth } from "@wymp/types";
@@ -37,7 +38,7 @@ export type ApiSpec<EndpointSpec extends GenericEndpointSpec = GenericEndpointSp
   [T in keyof EndpointSpec]: {
     method: keyof SimpleHttpRequestHandlerBasicInterface;
     endpoint: string;
-    parseBody?: (req: any, log: SimpleLoggerInterface) => any;
+    bodyParsers?: Array<(req: any, res: any, next: (e?: Error) => void) => any>;
     validate: (
       url: unknown,
       query: unknown,
@@ -110,11 +111,16 @@ export type ApiSpec<EndpointSpec extends GenericEndpointSpec = GenericEndpointSp
 export const applyApiSpec = <EndpointSpec extends GenericEndpointSpec = GenericEndpointSpec>(
   apiSpec: ApiSpec<EndpointSpec>,
   r: {
-    http: SimpleHttpRequestHandlerBasicInterface;
+    http: SimpleHttpRequestHandlerInterface;
     log: SimpleLoggerInterface;
   },
-  authStub?: Auth.ReqInfo | undefined
+  authStub?: Auth.ReqInfo | undefined,
+  globalBodyParsers?: Array<(req: any, res: any, next: (e: any) => void) => void>
 ): void => {
+  // Breaking this out into a more narrowly scoped variable is necessary for typescript
+  const basicHttp: SimpleHttpRequestHandlerBasicInterface = r.http;
+
+  // For each endpoint spec....
   for (const name in apiSpec) {
     // Get the current endpoint spec into a variable
     const endpointSpec = apiSpec[name];
@@ -124,120 +130,127 @@ export const applyApiSpec = <EndpointSpec extends GenericEndpointSpec = GenericE
         .padEnd(7)} ${endpointSpec.endpoint.toString()}`
     );
 
+    // Register global body parsers, if specified
+    if (globalBodyParsers) {
+      r.log.notice(`Registering ${globalBodyParsers.length} global body parser(s)`);
+      globalBodyParsers.forEach((parser) => r.http.use(parser));
+    }
+
     // Register the endpoint against the HTTP server object
-    r.http[endpointSpec.method](endpointSpec.endpoint, async (req, res, next) => {
-      try {
-        // 0. If we're stubbing out auth, do that
-        if (authStub) {
-          (req as any).auth = authStub;
-        }
+    basicHttp[endpointSpec.method](endpointSpec.endpoint, [
+      // If we've specified body parser, insert them as a middleware before the handler
+      ...(endpointSpec.bodyParsers ? endpointSpec.bodyParsers : []),
 
-        // 1. Make sure this is an authd request
-        assertAuthdReq(req);
-
-        // 2. Get a request-tagged logger
-        const log = logger(r.log, req, res);
-
-        // 3. Parse body if necessary
-        if (endpointSpec.parseBody) {
-          req.body = endpointSpec.parseBody(req, log);
-        }
-
-        // 4. Validate the request, returning standardized and types params
-        if (endpointSpec.hooks?.preValidate) {
-          await endpointSpec.hooks.preValidate(req, res, log);
-        }
-        let params = await endpointSpec.validate(req.params, req.query, req.body, req.auth, log);
-        if (endpointSpec.hooks?.postValidate) {
-          params = await endpointSpec.hooks.postValidate(params, req, res, log);
-        }
-
-        // 5. Authorize the request according to the authorization specification we've passed in or
-        // a custom authorization function.
-        if (endpointSpec.hooks?.preAuthorize) {
-          await endpointSpec.hooks.preAuthorize(params, req, res, log);
-        }
-        if (typeof endpointSpec.authorize === "function") {
-          await endpointSpec.authorize(params, req.auth, log);
-        } else {
-          authorize(req, endpointSpec.authorize, log);
-        }
-        if (endpointSpec.hooks?.postAuthorize) {
-          await endpointSpec.hooks.postAuthorize(params, req, res, log);
-        }
-
-        // 6. Run the primary handler for the request, returning a standardized response
-        if (endpointSpec.hooks?.preHandle) {
-          params = await endpointSpec.hooks.preHandle(params, req, res, log);
-        }
-        let response = await endpointSpec.handle(params, req.auth, log);
-        if (endpointSpec.hooks?.postHandle) {
-          response = await endpointSpec.hooks.postHandle(response, params, req, res, log);
-        }
-
-        // TODO: 7. Run include logic
-
-        // 8. Return a response
-
-        // Run pre-return hook
-        if (endpointSpec.hooks?.preReturn) {
-          response = await endpointSpec.hooks.preReturn(response, params, req, res, log);
-        }
-
-        // Apply headers
-        let applyJsonHeader = true;
-        if (response.headers) {
-          for (const h in response.headers) {
-            if (h.toLowerCase() === "content-type") {
-              applyJsonHeader = false;
-            }
-            res.set(h, response.headers[h]);
+      // Then run the handler
+      async (req, res, next) => {
+        try {
+          // 0. If we're stubbing out auth, do that
+          if (authStub) {
+            (req as any).auth = authStub;
           }
-        }
-        if (applyJsonHeader && response.type === "success") {
-          res.set("Content-Type", "application/json");
-        }
 
-        // Assemble and send response
-        if (response.type === "success") {
-          // On success, send a data response
-          let responseObject: Api.Response;
-          if (Array.isArray(response.data)) {
-            responseObject = {
-              t: "collection",
-              data: response.data,
-              // TODO: ...(included.length > 0 ? { included } : {}),
-              meta: Object.assign(
-                { pg: { size: 25, nextCursor: null, prevCursor: null } },
-                response.meta || {}
-              ),
-            };
-          } else if (response.data === null) {
-            responseObject = {
-              t: "null",
-              data: null,
-              ...(response.meta ? { meta: response.meta } : {}),
-            };
+          // 1. Make sure this is an authd request
+          assertAuthdReq(req);
+
+          // 2. Get a request-tagged logger
+          const log = logger(r.log, req, res);
+
+          // 3. Validate the request, returning standardized and types params
+          if (endpointSpec.hooks?.preValidate) {
+            await endpointSpec.hooks.preValidate(req, res, log);
+          }
+          let params = await endpointSpec.validate(req.params, req.query, req.body, req.auth, log);
+          if (endpointSpec.hooks?.postValidate) {
+            params = await endpointSpec.hooks.postValidate(params, req, res, log);
+          }
+
+          // 4. Authorize the request according to the authorization specification we've passed in or
+          // a custom authorization function.
+          if (endpointSpec.hooks?.preAuthorize) {
+            await endpointSpec.hooks.preAuthorize(params, req, res, log);
+          }
+          if (typeof endpointSpec.authorize === "function") {
+            await endpointSpec.authorize(params, req.auth, log);
           } else {
-            responseObject = {
-              t: "single",
-              data: response.data,
-              // TODO: ...(included.length > 0 ? { included } : {}),
-              ...(response.meta ? { meta: response.meta } : {}),
-            };
+            authorize(req, endpointSpec.authorize, log);
+          }
+          if (endpointSpec.hooks?.postAuthorize) {
+            await endpointSpec.hooks.postAuthorize(params, req, res, log);
           }
 
-          // Send it off
-          res.status(response.status).send(responseObject);
-        } else {
-          // Otherwise, for redirects, send a redirect response
-          res.status(response.status).send();
+          // 5. Run the primary handler for the request, returning a standardized response
+          if (endpointSpec.hooks?.preHandle) {
+            params = await endpointSpec.hooks.preHandle(params, req, res, log);
+          }
+          let response = await endpointSpec.handle(params, req.auth, log);
+          if (endpointSpec.hooks?.postHandle) {
+            response = await endpointSpec.hooks.postHandle(response, params, req, res, log);
+          }
+
+          // TODO: 6. Run include logic
+
+          // 7. Return a response
+
+          // Run pre-return hook
+          if (endpointSpec.hooks?.preReturn) {
+            response = await endpointSpec.hooks.preReturn(response, params, req, res, log);
+          }
+
+          // Apply headers
+          let applyJsonHeader = true;
+          if (response.headers) {
+            for (const h in response.headers) {
+              if (h.toLowerCase() === "content-type") {
+                applyJsonHeader = false;
+              }
+              res.set(h, response.headers[h]);
+            }
+          }
+          if (applyJsonHeader && response.type === "success") {
+            res.set("Content-Type", "application/json");
+          }
+
+          // Assemble and send response
+          if (response.type === "success") {
+            // On success, send a data response
+            let responseObject: Api.Response;
+            if (Array.isArray(response.data)) {
+              responseObject = {
+                t: "collection",
+                data: response.data,
+                // TODO: ...(included.length > 0 ? { included } : {}),
+                meta: Object.assign(
+                  { pg: { size: 25, nextCursor: null, prevCursor: null } },
+                  response.meta || {}
+                ),
+              };
+            } else if (response.data === null) {
+              responseObject = {
+                t: "null",
+                data: null,
+                ...(response.meta ? { meta: response.meta } : {}),
+              };
+            } else {
+              responseObject = {
+                t: "single",
+                data: response.data,
+                // TODO: ...(included.length > 0 ? { included } : {}),
+                ...(response.meta ? { meta: response.meta } : {}),
+              };
+            }
+
+            // Send it off
+            res.status(response.status).send(responseObject);
+          } else {
+            // Otherwise, for redirects, send a redirect response
+            res.status(response.status).send();
+          }
+        } catch (e) {
+          // On error, pass to the error handler function
+          next(e);
         }
-      } catch (e) {
-        // On error, pass to the error handler function
-        next(e);
-      }
-    });
+      },
+    ]);
   }
 };
 
